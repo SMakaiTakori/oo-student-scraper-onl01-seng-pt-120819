170bca25e1b362a92c4322eabbe7b763f8d6160f
S. Makai Takori <makait87@gmail.com>
2020-02-04 16:24:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do {|hash| Student.new(hash)}
+    students_array.each do { |hash| Student.new(hash) }
     end
   end
 
